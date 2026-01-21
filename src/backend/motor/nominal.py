d6a09694e8484ef75e7755c9dc7068db18a51989
Luan Freitas <luandkg@gmail.com>
2026-01-21 20:14:59 -0300

    feat(backend): reorganiza projeto
    Agora todo o backend fica isolado, é construido utilizando Python com framework Flask, a pasta de distribuição dos arquivos estáticos do frontend fica fora do backend

--- src/backend/motor/nominal.py
+++
@@ -1,5 +0,0 @@
-def detectar_substantivo_solicitacao(substantivos_solicitacao:list, linha_normalizada: str) -> str | None:
-    for s in substantivos_solicitacao:
-        if s in linha_normalizada:
-            return s
-    return None
